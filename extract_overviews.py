9039106bfec3b5f877200c8e8fd5015295b87bac
rossrco <rossrco@gmail.com>
2020-04-25 17:25:43 +0200

    add function to extract overview details

--- extract_overviews.py
+++ extract_overviews.py
@@ -21,9 +21,7 @@ def compose_url(area, new_ads):
     return url
 
 
-def extract_data(area, new_ads, verbose, min_wait, max_wait, max_pages):
-    url = compose_url(area, new_ads)
-
+def extract_ad_thumbnails(url, min_wait, max_wait, max_pages):
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
                                ca_certs=certifi.where())
 
@@ -37,8 +35,41 @@ def extract_data(area, new_ads, verbose, min_wait, max_wait, max_pages):
         parsed = BeautifulSoup(page.data.decode('utf-8'), 'html.parser')
         ads = parsed.findAll('article', {'class': 'ads__unit'})
         for a in ads:
-            print(a)
+            yield a
         i += 1
+
+
+def extract_thumbnail_details(ad):
+    thumbnail_details = {}
+    link_tag = ad.find('a', {'class': 'ads__unit__link'})
+    img_tag = ad.find('div', {'class': 'ads__unit__img__ratio'
+                                       ' img-format img-format--ratio3by2'
+                                       ' img-format--centered'})
+    addr_tag = ad.find('div', {'class': 'ads__unit__content__details'})
+    price_area_tag = ad.find('div', {'class': 'ads__unit__content__keys'})\
+        .findAll('div')
+
+    thumbnail_details['id'] = link_tag['id']
+    thumbnail_details['link'] = config['link_prefix'] + link_tag['href']
+    thumbnail_details['img_link'] = img_tag.img['src']
+    thumbnail_details['short_desc'] = link_tag.string.strip()
+    thumbnail_details['address_short'] = addr_tag.string.strip()
+    thumbnail_details['new_building'] = False
+    thumbnail_details['main_price'] = price_area_tag[1].string.strip()
+    thumbnail_details['viewed'] = time.strftime('%Y-%m-%d %X')
+    thumbnail_details['size'] = price_area_tag[0].string.strip()
+
+    return thumbnail_details
+
+
+def extract_data(area, new_ads, verbose, min_wait, max_wait, max_pages):
+    url = compose_url(area, new_ads)
+
+    ad_thumbnails = extract_ad_thumbnails(url, min_wait, max_wait, max_pages)
+    for ad in ad_thumbnails:
+        thumbnail_details = extract_thumbnail_details(ad)
+        print(thumbnail_details)
+        print('\n' * 2)
 
 
 if __name__ == '__main__':
